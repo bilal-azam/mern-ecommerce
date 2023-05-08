e3ff88fa611d83ad020b89a3fd8c338819397228
Bilal Azam <bilal.azam1@outlook.com>
2023-05-09 00:00:00 +0100

    Initialize eCommerce project ans setup basic structure

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('eCommerce API');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
