7f0cad2ce6304497a3e3947ce686c10daaf29cc1
Bilal Azam <bilal.azam1@outlook.com>
2023-05-13 00:00:00 +0100

    Add product listing and detail pages

---
+++ backend/routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Product = require('../models/Product');
+const router = express.Router();
+
+// Get all products
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+// Get single product
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ msg: 'Product not found' });
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+module.exports = router;
