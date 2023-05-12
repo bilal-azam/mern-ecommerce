29bdec0b004d1b5ce878d8bbda3d7b252d3f4e11
Bilal Azam <bilal.azam1@outlook.com>
2023-05-13 00:00:00 +0100

    Add product listing and detail pages

---
+++ backend/models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  imageUrl: { type: String, required: true },
+  category: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
