401bbed8ddabe22ceec45ef3fda0bd1924e6f9be
Bilal Azam <bilal.azam1@outlook.com>
2023-05-13 00:00:00 +0100

    Add product listing and detail pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProductList from './pages/ProductList';
+import ProductDetail from './pages/ProductDetail';
 import { Container } from 'react-bootstrap';
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
         </Routes>
       </Container>
     </Router>
