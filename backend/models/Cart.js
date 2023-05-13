4aa90c6b5e637428a05cfb15b0a1c439f3b5fa25
Bilal Azam <bilal.azam1@outlook.com>
2023-05-14 00:00:00 +0100

    Implement cart functionality and add cart routes

---
+++ backend/models/Cart.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const CartSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  items: [{
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    quantity: { type: Number, required: true }
+  }]
+});
+
+module.exports = mongoose.model('Cart', CartSchema);
