b378ef874a13ff911cb9d617c5e511cdeec3502a
Bilal Azam <bilal.azam1@outlook.com>
2023-05-17 00:00:00 +0100

    Integrate Stripe payment gateway for checkout

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,6 +2,9 @@ const express = require('express');
 const Order = require('../models/Order');
 const Cart = require('../models/Cart');
 const auth = require('../middleware/auth');
+const stripe = require('../config/stripe');
+
+const YOUR_DOMAIN = 'http://localhost:5000';
 const router = express.Router();
 
 // Create order
@@ -31,6 +34,27 @@ router.get('/', auth, async (req, res) => {
   try {
     const orders = await Order.find({ userId: req.user._id });
     res.json(orders);
+  } catch (err) {
+    res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+// Create order with payment intent
+router.post('/checkout', auth, async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.user._id });
+    if (!cart) return res.status(400).json({ msg: 'No items in cart' });
+
+    const total = cart.items.reduce((acc, item) => acc + item.quantity * item.productId.price, 0);
+
+    // Create Payment Intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total * 100, // Amount in cents
+      currency: 'usd',
+      metadata: { integration_check: 'accept_a_payment' }
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
   } catch (err) {
     res.status(500).json({ msg: 'Server error' });
   }
