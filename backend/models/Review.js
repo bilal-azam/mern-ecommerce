bf3d6643bc7d147ffd9c522c309b7bb564763a9a
Bilal Azam <bilal.azam1@outlook.com>
2023-05-20 00:00:00 +0100

    Add product reviews and ratings feature on Backend

---
+++ backend/models/Review.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  comment: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
