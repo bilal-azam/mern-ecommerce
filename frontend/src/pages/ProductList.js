8a138f4d25b68183be3c605a18c3a7f5211fac91
Bilal Azam <bilal.azam1@outlook.com>
2023-06-16 00:00:00 +0100

    Optimize product listing page for performance

--- frontend/src/pages/ProductList.js
+++ frontend/src/pages/ProductList.js
@@ -1,88 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Container, Form, ListGroup, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { fetchProducts } from '../redux/productsSlice';
 
 const ProductList = () => {
-  const [products, setProducts] = useState([]);
-  const [query, setQuery] = useState('');
-  const [category, setCategory] = useState('');
-  const [priceRange, setPriceRange] = useState('');
-  const [sort, setSort] = useState('');
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('http://localhost:5000/api/products/search', {
-        params: { query, category, priceRange }
-      });
-      setProducts(data);
-    };
+    useEffect(() => {
+        const loadProducts = async () => {
+            const data = await fetchProducts();
+            setProducts(data);
+        };
+        loadProducts();
+    }, []);
 
-    fetchProducts();
-  }, [query, category, priceRange]);
-
-  const handleFilterAndSort = async () => {
-    try {
-      const { data } = await axios.get('http://localhost:5000/api/products', {
-        params: { category, sort }
-      });
-      setProducts(data);
-    } catch (err) {
-      console.error('Error fetching products:', err);
-    }
-  };
-
-  return (
-    <Container>
-      <h2>Product List</h2>
-      <Form>
-        <Form.Group controlId="search">
-          <Form.Label>Search</Form.Label>
-          <Form.Control
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="category">
-          <Form.Label>Category</Form.Label>
-          <Form.Control
-            type="text"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="priceRange">
-          <Form.Label>Price Range</Form.Label>
-          <Form.Control
-            type="text"
-            value={priceRange}
-            onChange={(e) => setPriceRange(e.target.value)}
-            placeholder="e.g., 10-50"
-          />
-        </Form.Group>
-        <Form.Group controlId="sort">
-          <Form.Label>Sort by Price</Form.Label>
-          <Form.Control
-            as="select"
-            value={sort}
-            onChange={(e) => setSort(e.target.value)}
-          >
-            <option value="">None</option>
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-          </Form.Control>
-        </Form.Group>
-      </Form>
-      <ListGroup>
-        {products.map(product => (
-          <ListGroup.Item key={product._id}>
-            <h5>{product.name} - ${product.price}</h5>
-            <p>{product.description}</p>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </Container>
-  );
+    return (
+        <div>
+            <h1>Product Listing</h1>
+            <div className="product-grid">
+                {products.map((product) => (
+                    <div key={product.id} className="product-card">
+                        <h2>{product.name}</h2>
+                        <p>{product.description}</p>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default ProductList;
