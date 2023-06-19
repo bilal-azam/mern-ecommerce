41d8c8127153ddd0fcf34fb1fccf188c67363c1d
Bilal Azam <bilal.azam1@outlook.com>
2023-06-20 00:00:00 +0100

    Add admin dashboard with basic analytics

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -1,51 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Container, ListGroup, Button } from 'react-bootstrap';
+import React from 'react';
 
 const AdminDashboard = () => {
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    const fetchReviews = async () => {
-      try {
-        const { data } = await axios.get('http://localhost:5000/api/reviews', {
-          headers: { 'x-auth-token': localStorage.getItem('authToken') }
-        });
-        setReviews(data);
-      } catch (err) {
-        console.error('Error fetching reviews:', err);
-      }
-    };
-
-    fetchReviews();
-  }, []);
-
-  const handleModerate = async (reviewId, approved) => {
-    try {
-      await axios.post('http://localhost:5000/api/reviews/moderate', { reviewId, approved }, {
-        headers: { 'x-auth-token': localStorage.getItem('authToken') }
-      });
-      setReviews(reviews.filter(review => review._id !== reviewId));
-    } catch (err) {
-      console.error('Error moderating review:', err);
-    }
-  };
-
-  return (
-    <Container>
-      <h2>Admin Dashboard</h2>
-      <ListGroup>
-        {reviews.map(review => (
-          <ListGroup.Item key={review._id}>
-            <h5>{review.userId.name} - {review.rating} stars</h5>
-            <p>{review.comment}</p>
-            <Button onClick={() => handleModerate(review._id, true)}>Approve</Button>
-            <Button onClick={() => handleModerate(review._id, false)}>Reject</Button>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-    </Container>
-  );
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+            <div>
+                <h2>Sales Overview</h2>
+                {/* Add sales chart component here */}
+                <p>Total Sales: $X,XXX</p>
+            </div>
+            <div>
+                <h2>Product Performance</h2>
+                {/* Add product performance chart here */}
+                <p>Top Selling Product: XYZ</p>
+            </div>
+        </div>
+    );
 };
 
 export default AdminDashboard;
